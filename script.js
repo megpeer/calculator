4acaa8a17b02e097d1320f0e333e0c0f307e74d2
Meg Peer <megimu@gmail.com>
2024-06-01 13:56:12 -0400

    changed the event listener within JS file

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const display = document.querySelector(".display")
-const display2 = document.querySelector(".display2")
-const operator = document.querySelector(".operator")
+const buttons = document.querySelectorAll('input[type="button"');
 
-function appendToDisplay(input) {
-    display.textContent += input;
-}
+const input = document.getElementById('display')
 
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() { // when a button gets clicked, an function will fire
+      input.value += this.value; // 'this' is the button that gets clicked. We add its value to the input field
+    });
+  }
 
-function operate(num1, num2, operator) {
-if (operator = "+") {
-    return num1 + num2
-}
-if (operator = "-") {
-    return num1 - num2
-}
-if (operator = "x") {
-    return num1 * num2
-}
-else num1 / num2
-}
+// function operate(num1, num2, operator) {
+// if (operator = "+") {
+//     return num1 + num2
+// }
+// if (operator = "-") {
+//     return num1 - num2
+// }
+// if (operator = "x") {
+//     return num1 * num2
+// }
+// else num1 / num2
+// }
 
-function clearDisplay() {
-    display.textContent = "";
-}
+// function clearDisplay() {
+//     display.textContent = "";
+// }
