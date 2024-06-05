6e042430e11a08c2dae9107254db87f8c5008cb2
Meg Peer <megimu@gmail.com>
2024-06-05 08:35:24 -0400

    CSS chunked out, Decimal functionality massaged, division bug fix

--- script.js
+++ script.js
@@ -1,65 +1,49 @@
 
 const buttons = document.querySelectorAll('input[type="button"');
 const input = document.getElementById('display')
-
 let num1 = '';
 let num2 = '';
 let operator = null;
 let step = 0;
 let displayCurrent = document.getElementById('num1')
 let displayPrevious = document.getElementById('num2')
-// let numArray = [];
-// let numArray2 = [];
 let result = 0;
 
 function getNum(num) {
     if (step === 2) {
-        clearDisplay();   
+        false;   
     }
-
 
     if (step === 0 ){
     displayCurrent.append(num);
 
-    // num1 = Number(numArray.join(''));
-    // input.value = num1;
-    // console.log(num1, typeof(num1))
-    // return num1;
-
     }
     else if (step === 1) {
     
-
     displayCurrent.append(num);
-
-
 
     }
 }
 
+function decimalPressed() {
+        if (!displayCurrent.innerHTML.includes('.')) {
+            displayCurrent.append('.')
+        }
+}
+
+
 function getOp(op) {
-    if (step === 0){
     step = 1;
     operator = op;
     num1 = document.querySelector('#num1').innerText;
     displayPrevious.append(num1 + " " + operator);
     displayCurrent.innerHTML = "";
-    
-    // displayPrevious.append(" " + operator);
 
-    // displayCurrent.append("");
-
-
-
-    }
-    else {
-        return false;
-    }
 }
 
 
 function operate(num1, num2, operator) {
-    step = 0
+    step = 2
     num1 = parseFloat(document.querySelector('#num1').innerText);
     num2 = parseFloat(document.querySelector('#num2').innerText);
     displayCurrent.innerHTML = "";
@@ -78,8 +62,12 @@ if (operator === "*") {
     result = num1 * num2;
     displayCurrent.append(result);
 }
-else if (operator === "/") {
-    result = num1 / num2;
+else if (operator === "/" && num1 != 0) {
+    result = num2 / num1;
+    displayCurrent.append(result);
+}
+else if (operator === "/" && num1 == 0) {
+    result = "MAAAAAAAAAAAAATH!!";
     displayCurrent.append(result);
 }
 }
@@ -95,11 +83,14 @@ function clearDisplay() {
     num2 = null;
     operator = null;
     step = 0;
-    // numArray = [];
-    // numArray2 = [];
     result = 0
 }
 
 function del() {
-    displayCurrent.slice(0, -1);
+    displayCurrent.innerHTML = "";
 }
+
+
+// to do:
+// max char 10?
+// keyboard support
